20d0ac5eab1826c5fd76220a0318a95f8c73f27c
Wachukwu Emmanuel <immacurte1@gmail.com>
2022-01-28 04:49:04 +0100

    Failures breeds quality success

--- roles/configure-server/files/index.js
+++ roles/configure-server/files/index.js
@@ -1,6 +1,6 @@
 var http = require("http");
 var server = http.createServer(function (req, res) {
     res.writeHead(200);
-    res.end("Hello world!\n Failures breeds quality success - Wach");
+    res.end("Hello world!\n Failures breeds quality success\n#Wach");
 });
 server.listen(3000);
